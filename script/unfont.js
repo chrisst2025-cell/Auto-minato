a8c772709b4ffbb942714f47bbcfedaa52d0fc55
chrisst2025-cell <chrisst2025@gmail.com>
2026-07-22 00:27:23 +0100

    Update unfont.js

--- script/unfont.js
+++ script/unfont.js
@@ -1,15 +1,26 @@
 const { origin } = require("fontstyles");
 const chalk = require("chalk");
 
+const smallCapsMap = {
+  a:'ᴀ', b:'ʙ', c:'ᴄ', d:'ᴅ', e:'ᴇ', f:'ꜰ',
+  g:'ɢ', h:'ʜ', i:'ɪ', j:'ᴊ', k:'ᴋ', l:'ʟ',
+  m:'ᴍ', n:'ɴ', o:'ᴏ', p:'ᴘ', q:'ǫ', r:'ʀ',
+  s:'ꜱ', t:'ᴛ', u:'ᴜ', v:'ᴠ', w:'ᴡ', x:'x',
+  y:'ʏ', z:'ᴢ'
+};
+
+const toSmallCaps = t =>
+  (t || "").toLowerCase().split("").map(c => smallCapsMap[c] || c).join("");
+
 module.exports.config = {
   name: "unfont",
   version: "1.0.0",
-  hasPermssion: 1,
-  credits: "Yan Maglinte",
-  description: "Removes the bot's message's font by reacting to the bot's message",
-  usePrefix: true,
-  commandCategory: "message",
-  cooldowns: 2,
+  role: 1,
+  hasPrefix: true,
+  credits: "Chris st",
+  description: "Retire la police spéciale du message du bot en y réagissant",
+  usage: "Réagissez avec 👍 à un message du bot",
+  cooldown: 2,
 };
 
 module.exports.run = function ({ api, event }) {
@@ -21,7 +32,7 @@ module.exports.run = function ({ api, event }) {
         api.getMessage(event.threadID, event.messageID, (err, data) => {
           if (!err) {
             console.log(
-              chalk.yellow("Removed font for messageID:", data.messageID),
+              chalk.yellow("Police retirée pour le messageID:", data.messageID),
             );
             const results = data.body;
             api.editMessage(origin(results), event.messageID);
@@ -30,6 +41,6 @@ module.exports.run = function ({ api, event }) {
       }
     }
   } catch (error) {
-    console.log("An error occurred:", error);
+    console.log(toSmallCaps("Une erreur est survenue :"), error);
   }
 };
